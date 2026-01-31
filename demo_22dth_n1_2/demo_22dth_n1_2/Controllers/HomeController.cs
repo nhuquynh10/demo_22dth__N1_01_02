96478ed4364d7208147b8208c3fe22222f740834
DINHCONGMINH <dinhcongminh4424@gmail.com>
2026-01-31 08:34:27 +0700

    tonghaiso

--- demo_22dth_n1_2/demo_22dth_n1_2/Controllers/HomeController.cs
+++ demo_22dth_n1_2/demo_22dth_n1_2/Controllers/HomeController.cs
@@ -8,16 +8,18 @@ namespace demo_22dth_n1_2.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger1)
         {
-            _logger = logger;
+            _logger = logger1;
         }
 
         public IActionResult Index()
         {
             return View();
         }
-
+        int tonghaiso(int a, int b) { 
+            return a + b;
+        }
         public IActionResult Privacy()
         {
             return View();
---
+++ demo_22dth_n1_2/demo_22dth_n1_2/Controllers/api/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace demo_22dth_n1_2.Controllers.api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        public AdminController() { 
+            
+        }
+    }
+}
