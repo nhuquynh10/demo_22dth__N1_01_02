192918e8b84f9c89c9cc322cb77ce88c59c1eb89
toannguyen <toannguyen.programmer@gmail.com>
2026-01-31 08:03:20 +0700

    Init Project

---
+++ demo_22dth_n1_2/demo_22dth_n1_2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace demo_22dth_n1_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
